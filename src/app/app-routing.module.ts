55d9c69338180a61f161059db424a39aa4482c2f
Abanoub Nabil Tawfik <abanoub.nabil2016@gmail.com>
2023-02-23 15:00:25 +0200

    day 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentContactInfoComponent } from './department-contact-info/department-contact-info.component';
+import { DepartmentDetailsComponent } from './department-details/department-details.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/departments',pathMatch:'full'},
+  {path:'employees',component:EmployeeListComponent},
+  {path:'departments',component:DepartmentsComponent},
+  {
+   path:'department/:id',
+   component:DepartmentDetailsComponent,
+   children:[
+    {path:'contact',component:DepartmentContactInfoComponent},
+    {path:'overview',component:DepartmentOverviewComponent}
+   ]
+  },
+  {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)
+},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
